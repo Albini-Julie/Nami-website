f0dbb7ad3371f354749cc0959dc20b655c05a8f8
Julie <julie.albini@edu.univ-fcomte.fr>
2024-01-29 15:23:06 +0100

    feat(pays) : mise en place des icons de météo des pays

---
+++ components/icons/horloge.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg
+    viewBox="0 0 80 80"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    preserveAspectRatio="none"
+  >
+    <path
+      d="M40 0C18 0 0 18 0 40C0 62 18 80 40 80C62 80 80 62 80 40C80 18 62 0 40 0ZM40 72C22.4 72 8 57.6 8 40C8 22.4 22.4 8 40 8C57.6 8 72 22.4 72 40C72 57.6 57.6 72 40 72Z"
+      fill="black"
+    ></path>
+    <path d="M42 20H36V44L56.8 56.8L60 51.6L42 40.8V20Z" fill="black"></path>
+  </svg>
+</template>
