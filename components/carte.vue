82cf01357d55824788e0e6a773c19b87bd2c884b
Julie <julie.albini@edu.univ-fcomte.fr>
2024-02-01 14:40:07 +0100

    feat(carte-modifs) : mise en place des marqueurs qui s'adaptent aux couleurs du template

--- components/carte.vue
+++ components/carte.vue
@@ -10,23 +10,23 @@
 
       <!-- Ajouter des marqueurs -->
       <!--Marqeur de l'utilisateur-->
-      <LMarker :lat-lng="[latitudeUti, longitudeUti]">
+      <LMarker :lat-lng="[latitudeUti, longitudeUti]" :icon="customIconUti">
         <LTooltip :content="`Vous vous trouvez ici, à ${ville}`"></LTooltip>
       </LMarker>
       <!--Marqeur de la première capitale-->
-      <LMarker :lat-lng="[latitude1, longitude1]">
+      <LMarker :lat-lng="[latitude1, longitude1]" :icon="customIcon">
         <LTooltip
           :content="`Vous vous trouvez à ${distance1} km de ${capitale1}`"
         ></LTooltip>
       </LMarker>
       <!--Marqeur de la deuxième capitale-->
-      <LMarker :lat-lng="[latitude2, longitude2]">
+      <LMarker :lat-lng="[latitude2, longitude2]" :icon="customIcon">
         <LTooltip
           :content="`Vous vous trouvez à ${distance2} km de ${capitale2}`"
         ></LTooltip>
       </LMarker>
       <!--Marqeur de la troisème capitale-->
-      <LMarker :lat-lng="[latitude3, longitude3]">
+      <LMarker :lat-lng="[latitude3, longitude3]" :icon="customIcon">
         <LTooltip
           :content="`Vous vous trouvez à ${distance3} km de ${capitale3}`"
         ></LTooltip>
@@ -58,6 +58,71 @@ const props = defineProps([
   "longitudeUti",
   "ville",
 ]);
+
+const iconUti = ref("");
+console.log(props.secondaryColor);
+switch (props.secondaryColor) {
+  case "#00FFA3":
+    iconUti.value = "/marqueurs/vert_eau.png";
+    break;
+  case "#FF8A00":
+    iconUti.value = "/marqueurs/orange.png";
+    break;
+  case "#FFC700":
+    iconUti.value = "/marqueurs/orange_clair.png";
+    break;
+  case "#DB00FF":
+    iconUti.value = "/marqueurs/violet_clair.png";
+    break;
+  case "#001F8B":
+    iconUti.value = "/marqueurs/bleu_marine.png";
+    break;
+  case "#7A7A7A":
+    iconUti.value = "/marqueurs/gris.png";
+    break;
+  default:
+    iconUti.value = "/marqueurs/jaune_fonce.png";
+    break;
+}
+
+const customIconUti = L.icon({
+  iconUrl: iconUti.value, // Utilisez le chemin correct vers votre icône
+  iconSize: [30, 45], // Ajustez la taille de l'icône selon vos besoins
+  iconAnchor: [15, 45], // Ajustez l'ancre de l'icône selon vos besoins
+  popupAnchor: [0, -25], // Ajustez l'ancre du popup selon vos besoins
+});
+
+const iconCap = ref("");
+switch (props.primaryColor) {
+  case "#FF0099":
+    iconCap.value = "/marqueurs/rose.png";
+    break;
+  case "#AD00FF":
+    iconCap.value = "/marqueurs/violet.png";
+    break;
+  case "#001AFF":
+    iconCap.value = "/marqueurs/bleu_fonce.png";
+    break;
+  case "#FAFF00":
+    iconCap.value = "/marqueurs/jaune_clair.png";
+    break;
+  case "#FF63DD":
+    iconCap.value = "/marqueurs/rose_clair.png";
+    break;
+  case "#00FFFF":
+    iconCap.value = "/marqueurs/bleu_clair.png";
+    break;
+  default:
+    iconCap.value = "/marqueurs/rouge.png";
+    break;
+}
+
+const customIcon = L.icon({
+  iconUrl: iconCap.value, // Utilisez le chemin correct vers votre icône
+  iconSize: [30, 45], // Ajustez la taille de l'icône selon vos besoins
+  iconAnchor: [15, 45], // Ajustez l'ancre de l'icône selon vos besoins
+  popupAnchor: [0, -25], // Ajustez l'ancre du popup selon vos besoins
+});
 
 const zoom = ref(2);
 
